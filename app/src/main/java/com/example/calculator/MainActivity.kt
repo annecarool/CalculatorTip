1e0b39e6fc9aa336d056d2030115e5724b22981d
Aluno <annecarolzz@outlook.com>
2023-05-11 17:18:38 -0300

    20230511

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,17 +27,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calculator.ui.theme.CalculatorTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +66,14 @@ fun Backgroud(){
     Image(
         painter = painterResource(id = R.drawable.fundocalculator),
         contentDescription = null,
-        contentScale = ContentScale.FillWidth
-    )}
+        contentScale = ContentScale.Crop,
+        //modifier = Modifier
+            //.blur(
+                //radiusX = 3.dp,
+                //radiusY = 3.dp,
+                //edgeTreatment = BlurredEdgeTreatment(RoundedCornerShape(3.dp))
+            )
+    }
 
     Calculator()
 }
@@ -65,6 +83,11 @@ fun Backgroud(){
 fun Calculator(){
 
     var valorEntrada by remember { mutableStateOf("") }
+    var valorGorjeta by remember { mutableStateOf("0") }
+    var porcentagemGorjeta by remember { mutableStateOf("") }
+    var gorjeta by remember { mutableStateOf(0.0) } //lembrar o valor
+
+    gorjeta = CalcularGorjeta(valorEntrada, porcentagemGorjeta) //chamar valor e dar parâmetros
 
         Column (
             verticalArrangement = Arrangement.Top,
@@ -75,10 +98,10 @@ fun Calculator(){
             Text(
                 text = stringResource(R.string.title),
                 textAlign = TextAlign.Center,
-                fontSize = 60.sp,
+                fontSize = 30.sp,
                 color = Color(255, 255, 255),
-                fontFamily = FontFamily.Cursive,
-                fontWeight = FontWeight.W900,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Black,
                 modifier = Modifier
                     .padding(top = 40.dp)
             )
@@ -90,17 +113,35 @@ fun Calculator(){
                         },
                 onValueChange = {valorEntrada = it},
                 modifier = Modifier
-                    .padding(top = 40.dp))
+                    .padding(top = 40.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)) //tipo de teclado
+
+            TextField(
+                value = porcentagemGorjeta,
+                label = {
+                    Text(text = stringResource(R.string.porcent) )
+                },
+                onValueChange = {porcentagemGorjeta = it},
+                modifier = Modifier
+                    .padding(top = 40.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)) //tipo de teclado
 
             Text(
-                text = stringResource(R.string.result),
+                text = stringResource(R.string.result) + " ${NumberFormat.getCurrencyInstance().format(gorjeta)}", //juncão de texto + moeda do país
                 textAlign = TextAlign.Center,
                 fontSize = 20.sp,
                 color = Color(255, 255, 255),
                 fontFamily = FontFamily.Serif,
-                fontWeight = FontWeight.Normal,
+                fontWeight = FontWeight.W900,
                 modifier = Modifier
                     .padding(top = 40.dp)
-            )
+                    )
         }
     }
+
+fun CalcularGorjeta(
+    valorEntrada: String,
+    porcentagemGorjeta: String
+): Double { //string para double
+    return ((valorEntrada.toDoubleOrNull()?:0.0) * (porcentagemGorjeta.toDoubleOrNull()?:0.0) )/100 //calculo
+}
